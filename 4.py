85832a8676c8ed64eb10e170fc44e893b797f9f1
peach909 <99386746+peach909@users.noreply.github.com>
2022-04-05 20:54:48 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,16 @@
+print("введите числа")
+a=int(input("a1"))
+a2=int(input("a2"))
+a3=int(input("a3"))
+b=int(input("b1"))
+b2=int(input("b2"))
+ab=a+b
+t2=ab//10
+a2b2=a2+b2+t2
+t3=a2b2//10
+f1=(a3+t3)
+ff=a2+b2+t2
+f2=ff%10
+fff=a+b
+f3=fff%10
+print(f1,f2,f3)
